f80daa6f145f557aa082ad0bbce0ab76e888880b
SUMIT <sumitdogra3236@gmail.com>
2021-12-18 20:53:10 +0530

    go

--- img.go
+++ img.go
@@ -4,19 +4,25 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"fmt"
+    "io/ioutil"
+    "log"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+	root_src := "C:\Users\sumit\OneDrive\Pictures\Screenshots";
+	files, err := ioutil.ReadDir(root_src);
+	if err != nil {
+        log.Fatal(err)
+    }
+	for _, file := range files {
+        fmt.Println(file.Name(), file.IsDir())
+    }
+
 
 	w.ShowAndRun()
 }
+
