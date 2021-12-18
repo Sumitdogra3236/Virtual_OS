584e8352ef46c41cbd9f8bb54dd8a0f108ad2560
SUMIT <sumitdogra3236@gmail.com>
2021-12-18 20:54:31 +0530

    go

--- img.go
+++ img.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	"fmt"
     "io/ioutil"
     "log"
+	"fyne.io/fyne/v2/app"
+	// "fyne.io/fyne/v2/container"
+	// "fyne.io/fyne/v2/widget"
+	
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	root_src := "C:\Users\sumit\OneDrive\Pictures\Screenshots";
+	root_src := "C:\\Users\\sumit\\OneDrive\\Pictures\\Screenshots";
 	files, err := ioutil.ReadDir(root_src);
 	if err != nil {
         log.Fatal(err)
