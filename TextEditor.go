1ea5eed56dcd0d3e45c0dee4696bb10724adaee8
SUMIT <sumitdogra3236@gmail.com>
2021-12-19 16:55:55 +0530

    go

--- TextEditor.go
+++ TextEditor.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -21,14 +22,42 @@ func main() {
 		widget.NewLabel("Text Editor"),
 	),
 )
-content.Add(widget.NewButton("Add New File", func(){
-content.Add(widget.NewLabel("New File"+strconv.Itoa()))
+	content.Add(widget.NewButton("Add New File", func(){
+	content.Add(widget.NewLabel("New File"+strconv.Itoa(count)))
+	count++
 }))
 
+
+	input := widget.NewMultiLineEntry()
+	input.SetPlaceHolder("Enter text...")
+
+	input.Resize(fyne.NewSize(1200,1200))
+
+
+	saveBtn := widget.NewButton("Save text File", func() {
+		saveFileDialog := dialog.NewFileSave(
+			func(uc fyne.URIWriteCloser, _ error){
+				textData := []byte(input.Text)
+
+				uc.Write(textData)
+			},w)
+
+			saveFileDialog.SetFileName("New File"+strconv.Itoa(count-1) +".txt")
+
+			saveFileDialog.Show()
+	})
+
+
+	
 	w.SetContent(
 		container.NewVBox(
 			content,
+			input,
+			container.NewHBox(
+			saveBtn,
+			),
 		),
+		
 	)
 	w.ShowAndRun()
 }
